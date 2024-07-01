5dea1e19c3df25f7eedb6e3a809e0df9d942fd53
ModjadjiFrancis <modjadjitetelo.francis@gmail.com>
2024-07-01 23:57:23 +0200

    Collection

--- Summarizer.java
+++ Summarizer.java
@@ -1,8 +1,22 @@
 import numberrangesummarizer.NumberRangeSummarizer;
+import java.util.Collection;
+
 
 public class Summarizer implements NumberRangeSummarizer {
     
     public static void main (String args[]){
         System.out.println("Hello");
+
+        
     }
+
+    @Override
+    //method to collect input of integers
+    public Collection<Integer> collect (String input){
+
+        
+
+    }
+
+    
 }
