2994b29f83a1f7511f72dd206cd15173765cbfe5
ModjadjiFrancis <modjadjitetelo.francis@gmail.com>
2024-07-01 23:42:57 +0200

    Added a summariser class

---
+++ Summarizer.java
@@ -0,0 +1,8 @@
+import numberrangesummarizer.NumberRangeSummarizer;
+
+public class Summarizer implements NumberRangeSummarizer {
+    
+    public static void main (String args[]){
+        System.out.println("Hello");
+    }
+}
