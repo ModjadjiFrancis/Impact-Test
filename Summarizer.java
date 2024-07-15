865669d34450bb11437c38809da75005ed56c562
ModjadjiFrancis <modjadjitetelo.francis@gmail.com>
2024-07-15 07:14:18 +0200

    Seperating input into ranges

--- Summarizer.java
+++ Summarizer.java
@@ -1,18 +1,21 @@
+/* ASSUMPTIONS
+ * 
+ * Input entered will always be ordered. Assumption based on the ordered input example provided
+ * 
+ */
+
 import java.util.ArrayList;
 import java.util.Collection;
-
+import java.util.Collections;
 
 public class Summarizer implements NumberRangeSummarizer {
     
     public static void main (String args[]){
-        System.out.println("Hello");
         
         //using the interface 
-
         NumberRangeSummarizer numSum = new Summarizer();
-
         System.out.println(numSum.collect("1,2,3,4,5,6"));
-
+        System.out.println(numSum.summarizeCollection(null));
         
     }
 
@@ -27,11 +30,29 @@ public class Summarizer implements NumberRangeSummarizer {
             inputCol.add(Integer.parseInt(arrInput[i]));
         }
         return inputCol;
-
+        //An exception needs to be caught!
     }
 
     @Override
     public  String summarizeCollection(Collection<Integer> input){
+        
+        //Method to take input and summarise the collection
+
+        //Check for empty input 
+
+        if (input.isEmpty()){
+            return "Input is empty. Please enter valid input";
+        }
+
+        //Sorting the input 
+
+        ArrayList<Integer> sortedInput = new ArrayList<>(input);
+        Collections.sort(sortedInput);
+
+        //Seperating the input into ranges
+
+        
+        
         return "Hello";
     }
     
